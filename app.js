f30949e196a8ab744b8c408ca2075767cc7722e6
Hannah Magnusson <hannah-magnusson@live.nmit.ac.nz>
2026-03-09 11:00:33 +1300

    if/else statement practice

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello World")
+const userAge = 25;
+const hasMedicalClearance = true;
+
+if (userAge >= 21 && hasMedicalClearance) {
+  console.log('Gun Status', 'User is eligible to purchase a gun.');
+} else if (userAge >= 21 && !hasMedicalClearance) {
+  console.log('Gun Status', 'User is old enough, but needs medical clearance to purchase a gun.');
+} else {
+  console.log('Gun Status', 'User is not eligible to purchase a gun.');
+}
